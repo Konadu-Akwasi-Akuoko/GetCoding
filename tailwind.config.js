21de1158839fd52e26d04deda07dbb595aadc0e2
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-12-04 04:25:29 +0000

    Added JS and  and coded some few scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#FF6363',
+        primary: "#FF6363",
         secondry: {
-          100: '#E2E2D5',
-          200: '#888883'
-        }
-      }
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+
+      fontFamily: {
+        body_Nunito: ["Nunito", "sans-serif"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
